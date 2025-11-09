32656c32493723cc3d560bc7c068ecf7e9d2108a
Sam Groveman <samgroveman@outlook.com>
2025-11-09 11:12:56 -0500

    Added calibration, and moved to using GenericAnalogInput

--- src/DFWaterConductivity.cpp
+++ src/DFWaterConductivity.cpp
@@ -1,11 +1,11 @@
 #include "DFWaterConductivity.h"
 
-/// @brief Creates a soil moisture sensor
+/// @brief Creates a water conductivity sensor
 /// @param Name The device name
 /// @param EC_Pin The analog pin to use for the EC probe
 /// @param Temp_Pin The analog pin to use for the temp probe
 /// @param ConfigFile The name of the configuration file to use
-DFWaterConductivity::DFWaterConductivity(String Name, int EC_Pin, int Temp_Pin, String ConfigFile) : Sensor(Name) {
+DFWaterConductivity::DFWaterConductivity(String Name, int EC_Pin, int Temp_Pin, String ConfigFile) : GenericAnalogInput(Name, EC_Pin, ConfigFile) {
 	config_path = "/settings/sen/" + ConfigFile;
 	current_config.EC_Pin = EC_Pin;
 	current_config.Temp_Pin = Temp_Pin;
@@ -14,21 +14,21 @@ DFWaterConductivity::DFWaterConductivity(String Name, int EC_Pin, int Temp_Pin, 
 /// @brief Starts a Data Template object
 /// @return True on success
 bool DFWaterConductivity::begin() {
-	Description.parameterQuantity = 1;
-	Description.type = "Water Sensor";
-	Description.parameters = {"Conductivity", "Temperature"};
-	Description.units = {"uS/cm", "C"};
-	values.resize(Description.parameterQuantity);
 	bool result = false;
-	// Create settings directory if necessary
-	if (!checkConfig(config_path)){
-		// Set defaults
-		current_config.ADC_Volatage_mv = 3300;
-		current_config.ADC_Resolution = 4096;
-		result = saveConfig(config_path, getConfig());
-	} else {
-		// Load settings
-		result = setConfig(Storage::readFile(config_path), false);
+	if (GenericAnalogInput::begin()) {
+		Description.parameterQuantity = 1;
+		Description.type = "Water Sensor";
+		Description.parameters = {"Conductivity", "Temperature"};
+		Description.units = {"uS/cm", "C"};
+		values.resize(Description.parameterQuantity);
+		// Create settings directory if necessary
+		if (!checkConfig(config_path)){
+			// Set defaults
+			result = saveConfig(config_path, getConfig());
+		} else {
+			// Load settings
+			result = setConfig(Storage::readFile(config_path), false);
+		}
 	}
 	return result;
 }
@@ -36,32 +36,59 @@ bool DFWaterConductivity::begin() {
 /// @brief Takes a measurement
 /// @return True on success
 bool DFWaterConductivity::takeMeasurement() {
-	float Temp_Voltage = getAnalogMV(current_config.Temp_Pin);
-	float EC_Voltage = getAnalogMV(EC_PIN);
-	for (int i = 0; i < 9; i++) {
-		Temp_Voltage += getAnalogMV(current_config.Temp_Pin);
-		EC_Voltage += getAnalogMV(EC_PIN);
-		delay(5);
-	}
-	Temp_Voltage /= 10;
-	EC_Voltage /= 10;
+	analog_config.Pin = current_config.Temp_Pin;
+	float Temp_Voltage = getMVValue();
+
+	analog_config.Pin = current_config.EC_Pin;
+	float EC_Voltage = getMVValue();
+
 	float temp = ec_temp.convVoltagetoTemperature_C((float)Temp_Voltage/1000);
 	values[0] = ec.getEC_us_cm(EC_Voltage, temp);
 	values[1] = temp;
 	return true;
 }
 
+/// @brief Used to calibrate sensor
+/// @param step The calibration step to execute for multi-step calibration processes
+/// @return A tuple with the fist element as a Sensor::calibration_response and the second an optional message String accompanying the response
+std::tuple<Sensor::calibration_response, String> DFWaterConductivity::calibrate(int step) {
+	Logger.println("Calibrating EC sensor, step " + String(step));
+	std::tuple<Sensor::calibration_response, String> response;
+	int new_value;
+	// Disable averaging for calibration
+	bool average = analog_config.RollingAverage;
+	analog_config.RollingAverage = false;
+	switch (step) {
+		case 0:
+			response = { Sensor::calibration_response::NEXT, "Place sensor in calibration solution, then click next" };
+			break;
+		case 1:
+			uint16_t new_value = getMVValue();
+			for (int i = 0; i < 9; i++) {
+				new_value += getMVValue();
+				delay(50);
+			}
+			new_value /= 10;
+			current_config.K_Value = ec.calibrate(new_value, current_config.Calibration_Value);
+			// Apply calibration value
+			ec.~DFRobot_ECPRO();
+			ec = DFRobot_ECPRO(current_config.K_Value);
+			response = { Sensor::calibration_response::DONE, "Calibration complete, new K-value is: " + String(current_config.K_Value) };
+			break;
+		default:
+			response = { Sensor::calibration_response::ERROR, "No such calibration step: " + String(step) };
+			break;
+	}
+	// Re-enable averaging if needed
+	analog_config.RollingAverage = average;
+	return response;
+}
+
 /// @brief Gets the current config
 /// @return A JSON string of the config
 String DFWaterConductivity::getConfig() {
 	// Allocate the JSON document
-	JsonDocument doc;
-	// Assign current values
-	doc["Name"] = Description.name;
-	doc["EC_Pin"] = current_config.EC_Pin;
-	doc["Temp_Pin"] = current_config.Temp_Pin;
-	doc["ADC_Volatage_mv"] = current_config.ADC_Volatage_mv;
-	doc["ADC_Resolution"] = current_config.ADC_Resolution;
+	JsonDocument doc = addAdditionalConfig();
 
 	// Create string to hold output
 	String output;
@@ -75,33 +102,52 @@ String DFWaterConductivity::getConfig() {
 /// @param save If the configuration should be saved to a file
 /// @return True on success
 bool DFWaterConductivity::setConfig(String config, bool save) {
-	// Allocate the JSON document
-	JsonDocument doc;
-	// Deserialize file contents
-	DeserializationError error = deserializeJson(doc, config);
-	// Test if parsing succeeds.
-	if (error) {
-		Serial.print(F("Deserialization failed: "));
-		Serial.println(error.f_str());
-		return false;
+	if (GenericAnalogInput::setConfig(config, false)) {
+		// Allocate the JSON document
+		JsonDocument doc;
+		// Deserialize file contents
+		DeserializationError error = deserializeJson(doc, config);
+		// Test if parsing succeeds.
+		if (error) {
+			Serial.print(F("Deserialization failed: "));
+			Serial.println(error.f_str());
+			return false;
+		}
+		// Assign loaded values
+		current_config.EC_Pin = doc["Pin"].as<int>();
+		current_config.Temp_Pin = doc["Temp_Pin"].as<int>();
+		current_config.K_Value = doc["K_Value"].as<float>();
+		current_config.Calibration_Value = doc["Calibration_Value"].as<float>();
+
+		pinMode(current_config.Temp_Pin, INPUT);
+
+		// Apply calibration value
+		ec.~DFRobot_ECPRO();
+		ec = DFRobot_ECPRO(current_config.K_Value);
+
+		if (save) {
+			return saveConfig(config_path, config);
+		}
+		return true;
 	}
-	// Assign loaded values
-	Description.name = doc["Name"].as<String>();
-	current_config.EC_Pin = doc["EC_Pin"].as<int>();
-	current_config.Temp_Pin = doc["Temp_Pin"].as<int>();
-	current_config.ADC_Volatage_mv = doc["ADC_Volatage_mv"].as<float>();
-	current_config.ADC_Resolution = doc["ADC_Resolution"].as<int>();
-	pinMode(current_config.EC_Pin, INPUT);
-	pinMode(current_config.Temp_Pin, INPUT);
-	if (save) {
-		return saveConfig(config_path, config);
-	}
-	return true;
+	return false;
 }
 
-/// @brief Converts an analog reading to mV
-/// @param pin The pin number to read
-/// @return The reading in millivolts
-uint32_t DFWaterConductivity::getAnalogMV(int pin) {
-	return analogRead(pin) * current_config.ADC_Volatage_mv / current_config.ADC_Resolution;
+/// @brief Collects all the base class parameters and additional parameters
+/// @return a JSON document with all the parameters
+JsonDocument DFWaterConductivity::addAdditionalConfig() {
+	// Allocate the JSON document
+  	JsonDocument doc;
+	// Deserialize file contents
+	DeserializationError error = deserializeJson(doc, GenericAnalogInput::getConfig());
+	// Test if parsing succeeds.
+	if (error) {
+		Logger.print(F("Deserialization failed: "));
+		Logger.println(error.f_str());
+		return doc;
+	}
+	doc["Temp_Pin"] = current_config.Temp_Pin;
+	doc["K_Value"] = current_config.K_Value;
+	doc["Calibration_Value"] = current_config.Calibration_Value;
+	return doc;
 }
--- src/DFWaterConductivity.h
+++ src/DFWaterConductivity.h
@@ -10,19 +10,19 @@
  */
 
 #pragma once
-#include <Sensor.h>
+#include <GenericAnalogInput.h>
 #include <Storage.h>
 #include <ArduinoJson.h>
-#include <EEPROM.h>
 #include <DFRobot_ECPRO.h>
 
-class DFWaterConductivity : public Sensor {
+class DFWaterConductivity : public GenericAnalogInput {
 	public:
 		DFWaterConductivity(String Name, int EC_Pin = 36, int TEMP_Pin = 39, String ConfigFile = "DFWaterConductivity.json");
 		bool begin();
 		bool takeMeasurement();
 		String getConfig();
 		bool setConfig(String config, bool save);
+		std::tuple<Sensor::calibration_response, String> calibrate(int step);
 		
 	protected:
 		/// @brief Soil moisture sensor configuration
@@ -33,11 +33,11 @@ class DFWaterConductivity : public Sensor {
 			/// @brief The analog pin to use for the temperature probe
 			int Temp_Pin;
 
-			/// @brief The voltage used by the ADC in mv
-			int ADC_Volatage_mv;
+			/// @brief The K-value of the sensor
+			float K_Value = 1.0;
 
-			/// @brief The resolution of the ADC
-			int ADC_Resolution;
+			/// @brief The value of the calibration solution used
+			float Calibration_Value = 1413.0;
 		} current_config;
 
 		/// @brief Path to settings file
@@ -58,5 +58,5 @@ class DFWaterConductivity : public Sensor {
 		// EC EEPROM address for calibration
 		const int EC_ADDR = 0x20;
 
-		uint32_t getAnalogMV(int pin);
+		JsonDocument addAdditionalConfig();
 };
